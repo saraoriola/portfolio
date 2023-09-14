beb35764246242774091139a0d48c571bade4ab2
saraoriola <saraoriola@outlook.com>
2023-09-14 22:27:32 +0200

    Implementación de estilos de tipografía global en typography.scss

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -2,13 +2,9 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   fonts: {
-    body: "Montserrat, sans-serif", // Utiliza la fuente "Montserrat" que cargaste desde Google Fonts
-    heading: "Montserrat, sans-serif", // Utiliza la misma fuente para encabezados
+    body: "Montserrat, sans-serif",
+    heading: "Montserrat, sans-serif", 
   },
-  colors: {
-    // Define tus colores personalizados aquí
-  },
-  // Otras configuraciones de tu tema
 });
 
 export default theme;
