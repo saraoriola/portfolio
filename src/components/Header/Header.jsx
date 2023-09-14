a8448deaa9c6fca53fd9030e9597bc83e0c363f2
saraoriola <saraoriola@outlook.com>
2023-09-14 23:20:36 +0200

    Implementación de Enrutamiento y Modo Oscuro

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,38 +1,47 @@
-import React from "react";
-import { Box, Flex, Link, Text, Button, IconButton, Spacer } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+
+import { Box, Flex, Text, Button, IconButton, Spacer, useColorMode } from "@chakra-ui/react";
+import "./Header.scss";
+
 
 const Header = () => {
-  return (
-    <Box bg="blue.500" color="white" p={4}>
-      <Flex align="center">
-        <Text fontSize="xl" fontWeight="bold">
-          Mi Portfolio
-        </Text>
-        <Spacer />
-        <Box>
-          <Link to="/" marginRight={4}>
-            Inicio
-          </Link>
-          <Link to="/proyectos" marginRight={4}>
-            Proyectos
-          </Link>
-          <Link to="/contacto" marginRight={4}>
-            Contacto
-          </Link>
-        </Box>
-        <Spacer />
-        <Button colorScheme="teal" size="sm">
-          Iniciar sesión
-        </Button>
-        <IconButton
-          aria-label="Toggle dark mode"
-          icon={<i className="fa fa-moon"></i>}
-          size="sm"
-          ml={2}
-        />
-      </Flex>
-    </Box>
-  );
-};
-
-export default Header;
+    const { colorMode, toggleColorMode } = useColorMode();
+  
+    return (
+      <Box bg={colorMode === "light" ? "blue.500" : "gray.800"} color="white" p={4}>
+        <Flex align="center">
+          <Text fontSize="xl" fontWeight="bold">
+            Mi Portfolio
+          </Text>
+          <Spacer />
+          <Box>
+            <Link to="/" >
+              Inicio
+            </Link>
+            <Link to="/proyectos" >
+              Proyectos
+            </Link>
+            <Link to="/contacto" >
+              Contacto
+            </Link>
+          </Box>
+          <Spacer />
+          <Button colorScheme="teal" size="sm">
+            Iniciar sesión
+          </Button>
+          <IconButton
+            aria-label="Toggle dark mode"
+            icon={colorMode === "light" ? <i className="fa fa-moon"></i> : <i className="fa fa-sun"></i>}
+            size="sm"
+            ml={2}
+            onClick={toggleColorMode}
+          />
+        </Flex>
+      </Box>
+    );
+  };
+  
+  export default Header;
+  
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -3,7 +3,21 @@ import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
   fonts: {
     body: "Montserrat, sans-serif",
-    heading: "Montserrat, sans-serif", 
+    heading: "Montserrat, sans-serif",
+  },
+  initialColorMode: "light", 
+  useSystemColorMode: false, 
+  colors: {
+    light: {
+      primary: "#007bff",
+      secondary: "#6c757d",
+      
+    },
+    dark: {
+      primary: "#64a0e6",
+      secondary: "#ced4da",
+      
+    },
   },
 });
 
