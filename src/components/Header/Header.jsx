300ec679c93b6fd3d2a515f54ae487cf2ccbcb39
saraoriola <saraoriola@outlook.com>
2023-09-14 23:04:27 +0200

    Header - en funcionamiento

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Box, Flex, Link, Text, Button, IconButton, Spacer } from "@chakra-ui/react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <Box bg="blue.500" color="white" p={4}>
+      <Flex align="center">
+        <Text fontSize="xl" fontWeight="bold">
+          Mi Portfolio
+        </Text>
+        <Spacer />
+        <Box>
+          <Link to="/" marginRight={4}>
+            Inicio
+          </Link>
+          <Link to="/proyectos" marginRight={4}>
+            Proyectos
+          </Link>
+          <Link to="/contacto" marginRight={4}>
+            Contacto
+          </Link>
+        </Box>
+        <Spacer />
+        <Button colorScheme="teal" size="sm">
+          Iniciar sesión
+        </Button>
+        <IconButton
+          aria-label="Toggle dark mode"
+          icon={<i className="fa fa-moon"></i>}
+          size="sm"
+          ml={2}
+        />
+      </Flex>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
