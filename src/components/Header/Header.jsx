ba0a7668ba180a466d95d7aaf5f0600f9bc9b2f8
saraoriola <saraoriola@outlook.com>
2023-09-17 21:23:53 +0200

    Header adaptado a mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,40 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import { Box, Flex, Text, Button, IconButton, useColorMode, Spacer } from "@chakra-ui/react";
-import { Switch } from '@chakra-ui/react'
-import { FaMoon, FaSun } from "react-icons/fa";
-import "./Header.scss";
-
+import React from 'react';
+import { Box, Flex, Link, Button, Image } from '@chakra-ui/react';
 
 const Header = () => {
-    const { colorMode, toggleColorMode } = useColorMode();
-  
-    return (
-        <Box bg={colorMode === "light" ? "blue.500" : "gray.800"} color="white" p={4}>
-          <Flex align="center">
-            <Text fontSize="xl" fontWeight="bold">
-              Mi Portfolio
-            </Text>
-            <Box ml={4}>
-              <Link to="/">Inicio</Link>
-              <Link to="/proyectos">Proyectos</Link>
-              <Link to="/contacto">Contacto</Link>
-            </Box>
-            <Spacer/>
-            <Button colorScheme="teal" size="sm">
-              Iniciar sesión
-            </Button>
-            <Box ml={2}>
-            <Switch
-            isChecked={colorMode === "dark"}
-            onChange={() => toggleColorMode()}
-            size="lg"
-          />
-            </Box>
-          </Flex>
-        </Box>
-      );
-    };
-    
-    export default Header;
+  return (
+    <Flex
+      as="header"
+      align="center"
+      justify={['center', 'space-between']}
+      bg="transparent"
+      p={4}
+      flexWrap="wrap"
+      flexDirection={['column', 'row']}
+    >
+      <Image src="/logo.png" alt="Logo" h="40px" mb={[2, 0]} />
+      <Box
+        mt={[2, 0]}
+        fontSize={['sm', 'md']}
+        textAlign={['center', 'center']}
+      >
+        <Link href="#">Inicio</Link>
+        <Link href="#">About</Link>
+        <Link href="#">Contacto</Link>
+        <Button colorScheme="teal" ml={[0, 2]}>
+          Portfolio
+        </Button>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Header;
