e9c626a2ae4fbae92aa34fbaa79e6f3440cc3a6e
saraoriola <saraoriola@outlook.com>
2023-10-01 22:17:15 +0200

    Tittle ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,8 @@ import './Header.scss';
 
 function Header() {
   return (
-    <Box p={4} color="white">
-      <Flex align="center" justify="space-between">
+    <Box color="white">
+      <Flex align="center" justify="space-between" mt={4}ml={8} mr={8}>
         <Button className="custom-button">Sobre mi</Button>
         <Image alt="Logo" w="sm" src={logo} />
         <Button className="custom-button">Porfolio</Button>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,19 @@
-import { Text } from '@chakra-ui/react'
-import React from 'react'
-import "./Home.scss"
+import { Box, Image, Text } from '@chakra-ui/react';
+import React from 'react';
+import './Home.scss';
+import aste from "../../assets/images/aste.svg"
 
 const Home = () => {
   return (
-    <Text className='hola'>
-      Hola
-    </Text>
-  )
-}
+    <Box>
+      <div className='text-with-circle'>
+        <Text className='h1' ml={8} mt={8}>
+          GROWTH IS ABOUT VENTURING
+          <Image className='circle' src={aste}></Image>
+        </Text>
+      </div>
+    </Box>
+  );
+};
 
-export default Home
+export default Home;
